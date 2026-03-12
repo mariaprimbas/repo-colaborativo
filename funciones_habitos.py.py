4867e762ea22a340411a29f056dfd67614f13db3
okemmerervanini <okemmerervanini@udesa.edu.ar>
2026-03-12 14:23:59 -0300

    cargo las funciones

--- funciones_habitos.py.py
+++ funciones_habitos.py.py
@@ -6,6 +6,15 @@ Created on Thu Mar 12 13:41:04 2026
 """
 
 def registrar_habitos():
+    '''
+    Registrar los habitos de las personas 
+
+    Returns
+    -------
+    lista : list
+        Lista que guarda las actividades que hacen las personas.
+
+    '''
     actividades = input("Ingresa una actividad: ")
     lista =[]  
     lista.append(actividades) 
@@ -18,6 +27,20 @@ def registrar_habitos():
     return lista 
     
 def analizar_habitos(lista):
+    '''
+    Contar cuantas veces se realiza cada actividad 
+
+    Parameters
+    ----------
+    lista : list
+        lista que guarda las actividades.
+
+    Returns
+    -------
+    diccionario : dicc
+        Guarda como  las actividades y sus respectivas cantidades 
+
+    '''
     diccionario= {}
     for actividad in lista: 
         if actividad not in diccionario: 
